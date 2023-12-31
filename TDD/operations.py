e75689a37de3e0586cfb6fb3ce453ada1a3924a3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-31 14:05:16 +0000

    Added function documentation

--- TDD/operations.py
+++ TDD/operations.py
@@ -2,6 +2,18 @@
 
 
 def square_me(x: int) -> int:
+    """
+    Returns the square of an integer.
+
+    Args:
+        x (int): The integer number to square.
+
+    Returns:
+        int: The square of x.
+
+    Raises:
+        TypeError: When the value received is non-integer.
+    """
     if isinstance(x, str):
         raise TypeError("do not square strings")
 
