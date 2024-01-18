e261632960c65ec538ed4ee4cf81543b78273e43
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-18 17:38:28 +0000

    Added the header file

---
+++ linked_lists/list.h
@@ -0,0 +1,42 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct node_s - blueprint for a singly linked list
+ * @data: the data portion
+ * @next: the pointer to the next node
+ */
+typedef struct node_s
+{
+	int data;
+	struct node_s *next;
+} node_t;
+
+/**
+ * struct list_s - a blueprint for a linked list
+ * @head: a pointer to the head node
+ * @tail: a pointer to the tail node
+ * @size: the size (length) of the linked list
+ */
+typedef struct list_s
+{
+	node_t *head;
+	node_t *tail;
+	size_t size;
+} list_t;
+
+#define first(list) (list->head->data)
+#define last(list) (list->tail->data)
+
+/* function prototypes */
+
+list_t *init(void);
+void append(list_t *list, int data);
+void pop(list_t *list);
+void print_list(list_t *list);
+void print_element_at(list_t *list, int element);
+
+#endif /* LIST_H */
